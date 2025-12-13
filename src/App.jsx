7188c7e9c642c8581956df71dc7213f9b7598ebf
mkt-amaranzero <mkt@amaranzero.com>
2025-12-13 09:58:46 -0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -402,7 +402,7 @@ const LOGO_AMARA = "https://i.imgur.com/BQEQiWL.png";
                     <img 
                       src={LOGO_AMARA}
                       alt="Amara nzero" 
-                      className="h-24 object-contain"
+                      className="h-32 object-contain"
                     />
                     <div>
                       <h2 className="text-2xl font-bold text-gray-900">
